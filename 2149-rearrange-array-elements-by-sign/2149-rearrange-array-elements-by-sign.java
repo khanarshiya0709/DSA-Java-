87ee472072dd1db7aa4d30f6ff73cb5cc80f4803
arshiya <arshiyak0709@gmail.com>
2026-09-08 12:48:30 +0530

    Time: 5 ms (22.29%), Space: 177 MB (5.47%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,22 +1,36 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int n = nums.length;
-        int[] ans = new int[n];
-        int positive = 0;
-        int negative = 1;
-        for(int i=0; i<n; i++){
-            if(nums[i] < 0){
-                ans[negative] = nums[i];
-                negative +=2;
-            }else{
-                ans[positive] = nums[i];
-                positive += 2;
-            }
+    int n = nums.length;
+    int[] ans = new int[n];
+    int[] positive = new int[n];
+    int[] negative = new int[n];
+    int p = 0;
+    int neg = 0;
 
-            
-
+    for(int i=0; i<n; i++){
+        if(nums[i]>=0){
+            positive[p++] = nums[i];
+        }else{
+            negative[neg++] = nums[i];
         }
-        return ans;
+    }
+    int pSize = p;
+    int nSize = neg;
+    int i=0;
+    int j=0;
+    int idx = 0;
+    while(i<pSize && j<nSize){
+        ans[idx++] = positive[i++];
+        ans[idx++] = negative[j++];
+    }
+    while(i<pSize){
+        ans[idx++] = positive[i++];
+    }
+    while(j<nSize){
+        ans[idx++] = negative[j++];
+    }
+    return ans;
+    
         
     }
 }
